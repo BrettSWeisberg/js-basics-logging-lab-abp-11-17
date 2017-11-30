c3a1f96f4903a2c1228320e398a0eff96bc14480
brett weisberg <weisberg.brett@gmail.com>
2017-11-30 01:52:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ console.log('this code was called');
 console.log(driver);
 
 
-function createspy(){
-const spy = sinon.spy(console.log(;driver, 'is the driver variable name'));
-expect(spy.calledOnce).toBeTruthy();
-}
